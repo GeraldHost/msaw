ec3bcbf34ef898ec6d39cf2bf03f11ed8a4df680
Gerald <me@jacobford.co.uk>
2020-05-02 11:31:19 +0100

    feat(code): start code section

--- src/binary/codesection.rs
+++ src/binary/codesection.rs
@@ -1,23 +1,37 @@
 use std::io::Read;
 use crate::decoder::{DecodeError, DecodeResult, Decoder};
 
+type Local = (u32, u32);
+
+struct Code {
+    locals: Vec<Local>,
+    body: _,
+}
+
 pub struct CodeSection(Vec<Code>);
 
 impl CodeSection {
 
   // read locals
-  fn locals() {}
+  fn locals<R: Read>(decoder: &mut Decoder<R>) -> DecodeResult<Local> {
+    let count = decoder.varuint32()?;
+	let valtype = decoder.valtype()?;
+	Ok((count, valtype))
+  }
 
-  fn func<R: Read>(decoder: &mut Decoder<R>) {
+  fn func<R: Read>(decoder: &mut Decoder<R>) -> DecodeResult<Code> {
     let body_size = decoder.varuint32()?;
     let locals = decoder.vec(CodeSection::locals)?;
-    let code = decoder.decode(CodeSection::code)?;
-    
+    let body = decoder.decode(CodeSection::body)?;
+    Ok(Code {
+        locals: locals,
+        body: body,
+    })    
   }
 
   // read code section
   // read instructions
-  fn code() {}
+  fn body() {}
 
   pub fn decode<R: Read>(decoder: &mut Decoder<R>) -> DecodeResult<Self> {
     decoder.vec(CodeSection::func)
